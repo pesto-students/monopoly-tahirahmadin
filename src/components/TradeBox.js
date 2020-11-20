6cc276975c38deb8bf2d6335986e6caae3e6c24e
Tahir Ahmad <tahirahmad.in@gmail.com>
2020-11-20 16:07:10 +0530

    functionality implemented

--- src/components/TradeBox.js
+++ src/components/TradeBox.js
@@ -6,17 +6,27 @@ class TradeBox extends Component {
       <div>
         <div style={{ borderRadius: 10, border: '1px solid #212121', padding: 10, width: '300px' }} className="mt-5">
           <div className="d-flex flex-row justify-content-center">
-            <button className="btn btn-secondary mr-2" style={{ backgroundColor: '#615cbf', width: '100px' }}>
+            <button
+              className="btn btn-secondary mr-2"
+              style={{ backgroundColor: '#615cbf', width: '100px' }}
+              onClick={() => this.props.takeAction()}>
               Buy
             </button>
-            <button className="btn btn-secondary ml-2" style={{ backgroundColor: '#615cbf', width: '100px' }}>
-              Stats
+            <button
+              className="btn btn-secondary ml-2"
+              style={{ backgroundColor: '#615cbf', width: '100px' }}
+              onClick={() => this.props.passAction()}>
+              Pass
             </button>
           </div>
           <div
             className="d-flex justify-content-center my-2"
             style={{ height: '60px', width: '100%', border: '1px solid #212121' }}>
-            <div className="mr-1 mt-1">{gameBlocks[this.props.dice1 + this.props.dice2].pricetext}</div>
+            <div className="mr-1 mt-1">
+              {this.props.dice1 === -1
+                ? 'Roll the dice first'
+                : gameBlocks[this.props.dice1 + this.props.dice2].pricetext}
+            </div>
           </div>
         </div>
       </div>
--- src/screens/Game.js
+++ src/screens/Game.js
@@ -10,28 +10,44 @@ function Game(props) {
   const [hoverIndex, setHoverIndex] = useState(0);
   const [money1, setMoney1] = useState(1500);
   const [money2, setMoney2] = useState(1500);
-  const [dice1, setDice1] = useState(0);
-  const [dice2, setDice2] = useState(0);
+  const [dice1, setDice1] = useState(-1);
+  const [dice2, setDice2] = useState(-1);
+  const [spot1, setSpot1] = useState(0);
+  const [spot2, setSpot2] = useState(0);
+
+  const [diceCount, setDiceCount] = useState(0);
+  const [turnOf, setTurnOf] = useState(1);
   const [assets1, setAssets1] = useState([]);
   const [assets2, setAssets2] = useState([]);
 
-  const handleMouseHover = () => {
-    setIsHoverering(true);
-    setHoverIndex(0);
-  };
+  // const handleMouseHover = () => {
+  //   setIsHoverering(true);
+  //   setHoverIndex(0);
+  // };
 
-  const toggleHoverState = () => {
-    setIsHoverering(false);
+  // const toggleHoverState = () => {
+  //   setIsHoverering(false);
+  // };
+  let styles = {
+    turn: {
+      border: '1px solid #212121',
+      margin: 5,
+    },
+    notTurn: {
+      margin: 5,
+    },
   };
-
+  const checkTurn = () => {
+    return diceCount % 2 === 0;
+  };
   function blockCreator(element, index) {
     return (
       <div className="smallContainer">
         <p className="text">{element.name}</p>
-        <div className="ml-2">
+        <div className="ml-2 text-center">
           {' '}
-          {gameBlocks[dice1 + dice2] === element && (
-            <div style={{ backgroundColor: 'yellow', width: '20px', height: '20px' }}></div>
+          {gameBlocks[turnOf === 1 ? spot1 : spot2] === element && (
+            <div style={{ backgroundColor: turnOf === 1 ? 'green' : 'red', width: '15px', height: '15px' }}></div>
           )}
         </div>
       </div>
@@ -41,10 +57,10 @@ function Game(props) {
     return (
       <div className="columnContainer">
         <p className="text">{element.name}</p>
-        <div className="ml-2">
+        <div className="ml-2 text-center">
           {' '}
-          {gameBlocks[dice1 + dice2] === element && (
-            <div style={{ backgroundColor: 'yellow', width: '20px', height: '20px' }}></div>
+          {gameBlocks[turnOf === 1 ? spot1 : spot2] === element && (
+            <div style={{ backgroundColor: turnOf === 1 ? 'green' : 'red', width: '15px', height: '15px' }}></div>
           )}
         </div>
       </div>
@@ -56,7 +72,33 @@ function Game(props) {
     const dice2 = Math.floor(Math.random() * Math.floor(5)) + 1;
     setDice1(dice1);
     setDice2(dice2);
+    setDiceCount(diceCount + 1);
   }
+
+  const takeAction = () => {
+    let cost =
+      gameBlocks[dice1 + dice2].price === '' ? gameBlocks[dice1 + dice2 - 1].price : gameBlocks[dice1 + dice2].price;
+    if (turnOf === 1) {
+      let spotValue = spot1 + dice1 + dice2;
+      setMoney1(money1 - cost);
+      console.log(gameBlocks.length);
+      setSpot1(spotValue / gameBlocks.length >= 1 ? spotValue % gameBlocks.length : spotValue);
+      setTurnOf(2);
+    } else {
+      let spotValue = spot2 + dice1 + dice2;
+      setMoney2(money2 - gameBlocks[dice1 + dice2].price);
+
+      setSpot2(spotValue / gameBlocks.length >= 1 ? spotValue % gameBlocks.length : spotValue);
+      setTurnOf(1);
+    }
+  };
+  const passAction = () => {
+    if (turnOf === 1) {
+      setTurnOf(2);
+    } else {
+      setTurnOf(1);
+    }
+  };
   return (
     <div className="App container d-flex justify-content-center">
       <div className="float-left">
@@ -77,15 +119,15 @@ function Game(props) {
           </div>
           <div className="d-flex  flex-column justify-content-between">
             <DiceRoller handleRollDice={handleRollDice} dice1={dice1} dice2={dice2} />
-            <TradeBox dice1={dice1} dice2={dice2} />
+            <TradeBox dice1={dice1} dice2={dice2} takeAction={takeAction} passAction={passAction} />
             <div className="d-flex  flex-row justify-content-between">
-              <h6>
-                <span style={{ color: 'green' }}>• </span>
+              {checkTurn}
+              <h6 style={turnOf === 1 ? styles.turn : styles.notTurn}>
+                {turnOf === 1 ? <span style={{ color: 'green', width: 10 }}> • </span> : ''}
                 {props.player1}: $ {money1}
               </h6>
-              <h6>
-                {' '}
-                <span style={{ color: 'red' }}>• </span>
+              <h6 style={turnOf === 2 ? styles.turn : styles.notTurn}>
+                {turnOf === 2 ? <span style={{ color: 'red', width: 10 }}> • </span> : ''}
                 {props.player2}: $ {money2}
               </h6>
             </div>
